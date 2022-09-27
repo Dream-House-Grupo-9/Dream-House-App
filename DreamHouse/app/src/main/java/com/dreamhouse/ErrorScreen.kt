7bb97ac58eb0baab30b7678e677bacab35671fd3
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-09-27 14:10:23 -0300

    Feature: adição da tela de erro

---
+++ DreamHouse/app/src/main/java/com/dreamhouse/ErrorScreen.kt
@@ -0,0 +1,11 @@
+package com.dreamhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ErrorScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.error_screen)
+    }
+}
