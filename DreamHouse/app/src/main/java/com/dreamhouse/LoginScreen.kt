b916b6017877ea0684ad00458a3f8d0dfbfbd05b
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-09-26 22:09:14 -0300

    Feature: login screen

---
+++ DreamHouse/app/src/main/java/com/dreamhouse/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.dreamhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+}
