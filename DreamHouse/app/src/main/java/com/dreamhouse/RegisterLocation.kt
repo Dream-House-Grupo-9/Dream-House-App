d41e88ef5135a0477583177b80e9b0abe574c864
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-10-07 17:32:07 -0300

    Feature: tela de cadastro de locação

---
+++ DreamHouse/app/src/main/java/com/dreamhouse/RegisterLocation.kt
@@ -0,0 +1,11 @@
+package com.dreamhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterLocation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_location)
+    }
+}
