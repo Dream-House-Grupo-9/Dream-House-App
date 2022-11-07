a43a37bda56c06ece53c1929a55a9c05bd657d6e
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-11-07 18:22:24 -0300

    feature: detalhes da locação

---
+++ DreamHouse/app/src/main/java/com/dreamhouse/LocationDetail.kt
@@ -0,0 +1,11 @@
+package com.dreamhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_detail)
+    }
+}
