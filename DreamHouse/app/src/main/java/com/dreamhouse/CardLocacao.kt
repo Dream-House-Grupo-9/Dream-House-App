80eb952dea08e04ee75a9a6404dcf1be762e0065
FelipeAmorim720 <felipe.reis@bandtec.com.br>
2022-12-05 22:11:31 -0300

    [refactor] removendo importacoes nao usadas

--- DreamHouse/app/src/main/java/com/dreamhouse/CardLocacao.kt
+++ DreamHouse/app/src/main/java/com/dreamhouse/CardLocacao.kt
@@ -1,26 +1,15 @@
 package com.dreamhouse
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.dreamhouse.adapters.ImovelGridAdapter
 import com.dreamhouse.databinding.ActivityMainBinding
 import com.dreamhouse.models.LocacaoListCard
-import com.dreamhouse.models.UsuarioLogin
 import com.dreamhouse.network.ImovelApiService
 import com.dreamhouse.rest.Rest
-import com.dreamhouse.services.LocacaoService
-import com.dreamhouse.services.UsuarioService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.create
 
 class CardLocacao : AppCompatActivity() {
 
