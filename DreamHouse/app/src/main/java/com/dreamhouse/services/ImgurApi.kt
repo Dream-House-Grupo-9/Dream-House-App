f8e6f4bd4e40ed546032ed850a75c23674aacab7
SamuAlmeida <samuel.bandtec@outlook.com.br>
2022-12-09 16:52:50 -0300

    adicionando endpoint de captura de anuncio

--- DreamHouse/app/src/main/java/com/dreamhouse/services/ImgurApi.kt
+++ DreamHouse/app/src/main/java/com/dreamhouse/services/ImgurApi.kt
@@ -1,8 +1,8 @@
 package com.dreamhouse.services
 
 import com.dreamhouse.models.ImgurUploadJson
-import retrofit2.Call
 import okhttp3.MultipartBody
+import retrofit2.Call
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -12,5 +12,6 @@ interface ImgurApi {
 
     @Multipart
     @POST("/3/image")
-    fun postImage(@Header("Authorization") clientId: String, @Part image: MultipartBody.Part): Call<ImgurUploadJson>
+    fun postImage(@Header("Authorization") clientId: String,
+                  @Part image: MultipartBody.Part): Call<ImgurUploadJson>
 }
--- DreamHouse/app/src/main/java/com/dreamhouse/services/LocacaoService.kt
+++ DreamHouse/app/src/main/java/com/dreamhouse/services/LocacaoService.kt
@@ -1,13 +1,18 @@
 package com.dreamhouse.services
 
+import com.dreamhouse.Anuncio
 import com.dreamhouse.models.Locacao
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface LocacaoService {
 
-    @POST("/anuncios")
+    @POST("/cadastrar/anuncio")
     fun createLocacao(@Body locacao: Locacao): Call<Any>
+
+    @GET("/anuncios")
+    fun getAnuncio(): Call<List<Anuncio>>
 
 }
